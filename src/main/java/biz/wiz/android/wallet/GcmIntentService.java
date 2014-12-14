a97feaad71d0077b196df7c001d121c7950cf520
J. Maurice <j@wiz.biz>
2014-12-14 20:09:03 +0900

    Call SendCoinsActivity from GCM handler

--- src/main/java/biz/wiz/android/wallet/GcmIntentService.java
+++ src/main/java/biz/wiz/android/wallet/GcmIntentService.java
@@ -10,6 +10,7 @@ import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.os.SystemClock;
 import android.support.v4.app.NotificationCompat;
@@ -17,6 +18,8 @@ import android.util.Log;
 
 import org.bitcoinj.core.Address;
 import org.bitcoinj.core.AddressFormatException;
+import org.bitcoinj.uri.BitcoinURI;
+import org.bitcoinj.uri.BitcoinURIParseException;
 
 import biz.wiz.android.wallet.data.PaymentIntent;
 import biz.wiz.android.wallet.ui.WalletActivity;
@@ -60,20 +63,50 @@ public class GcmIntentService extends IntentService {
                 sendNotification("Deleted messages on server: " + extras.toString());
                 // If it's a regular GCM message, do some work.
             } else if (GoogleCloudMessaging.MESSAGE_TYPE_MESSAGE.equals(messageType)) {
-
                 // Post notification of received message.
-                sendNotification(extras.getString("to"));
-                Log.i(TAG, "Received: " + extras.getString("message"));
+                sendNotification(extras.getString("address"));
 
-                Intent sendIntent = new Intent(this, SendCoinsActivity.class);
-                sendIntent.putExtras(extras);
-                sendIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-                startActivity(sendIntent);
-
+                // parse and log
+                String address = extras.getString("address");
+                Long amount = Long.valueOf(extras.getString("amount"));
+                Log.i(TAG, "Received send request to " + address + " for " + amount + " satoshi");
+                doSendIntent(address, amount);
             }
         }
         // Release the wake lock provided by the WakefulBroadcastReceiver.
         GcmBroadcastReceiver.completeWakefulIntent(intent);
+    }
+
+    private static final int SATOSHIS_PER_COIN = 100000000;
+
+    private void doSendIntent(final String address, final Long amount)
+    {
+        try {
+            // build bitcoin uri from arguments
+            StringBuilder uri = new StringBuilder("bitcoin:");
+            if (address != null)
+                uri.append(address);
+            if (amount != null)
+                uri.append("?amount=").append(String.format("%d.%08d", amount / SATOSHIS_PER_COIN, amount % SATOSHIS_PER_COIN));
+
+            // pass uri string to bitcoinj library
+            BitcoinURI buri = new BitcoinURI(uri.toString());
+
+            // convert bitcoinj object to intent data
+            PaymentIntent paymentIntent = PaymentIntent.fromBitcoinUri(buri);
+
+            // create intent
+            Intent sendIntent = new Intent(this, SendCoinsActivity.class);
+            // add payment data as extra
+            sendIntent.putExtra(SendCoinsActivity.INTENT_EXTRA_PAYMENT_INTENT, paymentIntent);
+            // start intent
+            sendIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            startActivity(sendIntent);
+        }
+        catch (BitcoinURIParseException e)
+        {
+            Log.i(TAG, "Failed parsing send request to " + address + " for " + amount + " satoshi: " + e.toString());
+        }
     }
 
     // Put the message into a notification and post it.
