ab3b7a219643f62283fa1ed849cb429f365729eb
magmahindenburg <magma@moar.se>
2015-01-08 23:51:45 +0900

    Quick edit to receive a seralized transaction from GCM

--- src/main/java/biz/wiz/android/wallet/GcmIntentService.java
+++ src/main/java/biz/wiz/android/wallet/GcmIntentService.java
@@ -18,8 +18,25 @@ import android.util.Log;
 
 import org.bitcoinj.core.Address;
 import org.bitcoinj.core.AddressFormatException;
+import org.bitcoinj.core.ECKey;
+import org.bitcoinj.core.NetworkParameters;
+import org.bitcoinj.core.Sha256Hash;
+import org.bitcoinj.core.Transaction;
+import org.bitcoinj.core.TransactionInput;
+import org.bitcoinj.crypto.TransactionSignature;
+import org.bitcoinj.params.MainNetParams;
+import org.bitcoinj.script.Script;
+import org.bitcoinj.script.ScriptBuilder;
+import org.bitcoinj.script.ScriptChunk;
 import org.bitcoinj.uri.BitcoinURI;
 import org.bitcoinj.uri.BitcoinURIParseException;
+
+import java.io.UnsupportedEncodingException;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 
 import biz.wiz.android.wallet.data.PaymentIntent;
 import biz.wiz.android.wallet.ui.WalletActivity;
@@ -64,13 +81,17 @@ public class GcmIntentService extends IntentService {
                 // If it's a regular GCM message, do some work.
             } else if (GoogleCloudMessaging.MESSAGE_TYPE_MESSAGE.equals(messageType)) {
                 Log.i(TAG, "Got cloud message: " + extras.toString());
-                // parse and log
-                String address = extras.getString("address");
-                Long amount = Long.valueOf(extras.getString("amount"));
-                Log.i(TAG, "Received send request to " + address + " for " + amount + " satoshi");
+                Log.i(TAG, "Received tx " + extras.getString("tx"));
+                Transaction transaction = new Transaction(MainNetParams.get(), hexStringToByteArray(extras.getString("tx"))); // Update network parameters to global setting
+                Log.i(TAG, "Received tx hash " + transaction.getHashAsString());
+
+                signTransaction(transaction);
+
+                Log.i(TAG, "New tx hash " + transaction.getHashAsString());
+                Log.i(TAG, "New tx " + byteArrayToHex(transaction.bitcoinSerialize()));
 
                 // Post notification of received message.
-                sendNotification(extras.getString("address"), prepareSendIntent(address, amount));
+                // sendNotification(extras.getString("address"), prepareSendIntent(address, amount));
             }
         }
         // Release the wake lock provided by the WakefulBroadcastReceiver.
@@ -133,5 +154,99 @@ public class GcmIntentService extends IntentService {
         }
 
         mNotificationManager.notify(NOTIFICATION_ID, mBuilder.build());
+    }
+
+    private byte[] hexStringToByteArray(String s) {
+        int len = s.length();
+        byte[] data = new byte[len / 2];
+        for (int i = 0; i < len; i += 2) {
+            data[i / 2] = (byte) ((Character.digit(s.charAt(i), 16) << 4)
+                    + Character.digit(s.charAt(i+1), 16));
+        }
+        return data;
+    }
+
+    private void signTransaction(Transaction spendTx)
+    {
+        List<TransactionInput> inputList = spendTx.getInputs();
+        Iterator<TransactionInput> inputIterator = inputList.iterator();
+        String inputAddress = "";
+        int i=0;
+
+        while (inputIterator.hasNext())
+        {
+            TransactionInput inputToBeSigned = inputIterator.next();
+            // Get the input chunks
+            Script inputScript = inputToBeSigned.getScriptSig();
+            List<ScriptChunk> scriptChunks = inputScript.getChunks();
+
+            // Create a list of all signatures. Start by extracting the existing ones from the list of script schunks.
+            // The last signature in the script chunk list is the redeemScript
+            List<TransactionSignature> signatureList = new ArrayList<TransactionSignature>();
+            Iterator<ScriptChunk> iterator = scriptChunks.iterator();
+            Script redeemScript = null;
+
+            while (iterator.hasNext())
+            {
+                ScriptChunk chunk = iterator.next();
+                System.out.println(chunk.toString());
+                System.out.println(iterator.hasNext());
+
+                if (iterator.hasNext() && chunk.opcode != 0)
+                {
+                    TransactionSignature transactionSignarture = TransactionSignature.decodeFromBitcoin(chunk.data, false);
+                    signatureList.add(transactionSignarture);
+                } else
+                {
+                    redeemScript = new Script(chunk.data);
+                }
+            }
+
+            // Create the sighash using the redeem script
+            Sha256Hash sighash = spendTx.hashForSignature(i, redeemScript, Transaction.SigHash.ALL, false);
+            ECKey.ECDSASignature secondSignature;
+
+            // Take out the key and sign the signhash
+            ECKey key2 = createKeyFromSha256Passphrase("Super secret key 2");
+            secondSignature = key2.sign(sighash);
+
+            // Add the second signature to the signature list
+            TransactionSignature transactionSignature = new TransactionSignature(secondSignature, Transaction.SigHash.ALL, false);
+            signatureList.add(transactionSignature);
+
+            // Rebuild p2sh multisig input script
+            inputScript = ScriptBuilder.createP2SHMultiSigInputScript(signatureList, redeemScript);
+            spendTx.getInput(i).setScriptSig(inputScript);
+            i++;
+        }
+    }
+
+    /**
+     * Method to convert a passphrase similar to brainwallet.org, to a bitcoin private key. This method of creating an ECKey is deprecated
+     * @param secret
+     * @return
+     */
+    public static ECKey createKeyFromSha256Passphrase(String secret) {
+        byte[] hash = null;
+
+        try {
+            MessageDigest md = MessageDigest.getInstance("SHA-256");
+            md.update(secret.getBytes("UTF-8"));
+            hash = md.digest();
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
+        @SuppressWarnings("deprecation")
+        ECKey key = new ECKey(hash, (byte[])null);
+        return key;
+    }
+
+    private String byteArrayToHex(byte[] a) {
+        StringBuilder sb = new StringBuilder(a.length * 2);
+        for(byte b: a)
+            sb.append(String.format("%02x", b & 0xff));
+        return sb.toString();
     }
 }
